3272f9d36bab4d078a7e5d8209165ca1b039d9a2
Heitor SF <85591540+HeitorSF5@users.noreply.github.com>
2023-07-19 05:21:08 -0300

    Update server.js PORT variable call
    changing .env variable from DB_PORT to PORT
    
    Hopefully Railway won't complain anymore

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,6 @@ const app = require('./app')
 
 require('dotenv').config();
 
-const PORT = process.env.DB_PORT || 3334
+const PORT = process.env.PORT || 3334
 
 app.listen(PORT, () => console.log('running on Port', PORT));
