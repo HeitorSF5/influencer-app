f74e0dfb495552a11650ff069b3eeb4015cdead4
Heitor Souza <heitor.sf5@gmail.com>
2023-07-20 02:19:10 -0300

    checking if increasing connection pool size helps anything. Mysql connections are returning 503

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -7,6 +7,7 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
+  connectionLimit: 20,
 });
 
 module.exports = connection;
