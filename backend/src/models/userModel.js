f7b847788f335ee87654742314091e0d183fd6a6
Heitor Souza <heitor.sf5@gmail.com>
2023-07-20 02:40:57 -0300

    try catching the mysql connection

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -5,12 +5,17 @@ const loginModel = async (user) => {
   const { username, password } = user;
   const hashedPassword = md5(password);
   console.log('In model! Making connection to MySQL now.')
-  const [query] = await connection.execute(
-    'SELECT username FROM Users WHERE username=? AND password=?',
-    [username, hashedPassword],
-  );
-  if(query.length > 0) return true;
-  else return false;
+
+  try {
+    const [query] = await connection.execute(
+      'SELECT username FROM Users WHERE username=? AND password=?',
+      [username, hashedPassword],
+    );
+    if(query.length > 0) return true;
+    else return false;
+  } catch(err) {
+    console.log('--- ERROR! ---', err)
+  }
 }
 
 // to do: Remodel DB users table to have "priviliges" column (user / admin / mod / etc)
