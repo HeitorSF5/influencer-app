7c9846ef2c905adf33b8b5609cb83ae6d33ebe31
Heitor Souza <heitor.sf5@gmail.com>
2023-07-20 01:00:58 -0300

    added HTTPS request compatibility. Hardcoded certificates

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
-const app = require('./app')
-
 require('dotenv').config();
+const PORT = process.env.PORT || 3333;
+const app = require('./app');
+const https = require('https');
+const fs = require('fs');
 
-const PORT = process.env.PORT || 3333
+const privateKey = fs.readFileSync('./sslcert/server.key', 'utf8');
+const certificate = fs.readFileSync('./sslcert/server.cert', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
 
-app.listen(PORT, () => console.log('running on Port', PORT));
+
+httpsServer.listen(PORT, () => console.log('running on HTTPS Port', PORT));
