a3b30c7883156cfea7e5a3e2f2aaac76c4a3ddc6
Heitor Souza <heitor.sf5@gmail.com>
2023-07-20 01:31:14 -0300

    undoing HTTPS implementation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 3333;
 const app = require('./app');
-const https = require('https');
-const fs = require('fs');
+// const https = require('https');
+// const fs = require('fs');
 
-const privateKey = fs.readFileSync('./sslcert/server.key', 'utf8');
-const certificate = fs.readFileSync('./sslcert/server.cert', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-const httpsServer = https.createServer(credentials, app);
+// const privateKey = fs.readFileSync('./sslcert/server.key', 'utf8');
+// const certificate = fs.readFileSync('./sslcert/server.cert', 'utf8');
+// const credentials = { key: privateKey, cert: certificate };
+// const httpsServer = https.createServer(credentials, app);
 
-
-httpsServer.listen(PORT, () => console.log('running on HTTPS Port', PORT));
+app.listen(PORT, () => console.log('running on HTTPS Port', PORT));
+// httpsServer.listen(PORT, () => console.log('running on HTTPS Port', PORT));
