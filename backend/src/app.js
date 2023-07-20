96ef9f61f51816943c803de5a35421c0ea474b08
Heitor Souza <heitor.sf5@gmail.com>
2023-07-20 03:49:38 -0300

    fixing cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,11 +7,12 @@ const path = require('path');
 const app = express();
 const frontEndBuildPath = path.join(__dirname, '../frontend-build');
 
-const corsOptions = { 
-  origin: 'http://localhost:4200'
-}
+// const corsOptions = { 
+//   origin: 'http://localhost:4200'
+// }
+// app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.static(frontEndBuildPath));
 app.use(loginRouter);
